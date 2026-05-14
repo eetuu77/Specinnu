9a9249a8146329b09fb6b5b8bac7282b3e3d3743
eetuu77 <eetulehto2013@gmail.com>
2026-05-14 20:24:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const CHANNEL_ID = "1503792252747518062"
 const CHANNEL_ID = "1503792252747518062";
-if (message.channel.id !== CHANNEL_ID) return;
 client.on('messageCreate', async (message) => {
-
+if (message.channel.id !== CHANNEL_ID) return;
   if (message.channel.id !== CHANNEL_ID) return;
 
   if (message.author.bot) return;
