f8fafe496cba3584cff971f87cee41d2960c98e2
eetuu77 <eetulehto2013@gmail.com>
2026-05-14 15:23:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-const CHANNEL_ID = "1503792252747518062";
-
-if (message.channel.id !== CHANNEL_ID) return;
+const CHANNEL_ID = "1503792252747518062";if (message.channel.id !== 1503792252747518062) return;
